11ee21f5fd0f38d8b2a66e4d5b9dd7dc3f97acd7
Rachel Cameron <rachleecameron@gmail.com>
2021-09-13 23:55:23 -0400

    Commit

--- src/main/java/exercise20.java
+++ src/main/java/exercise20.java
@@ -8,5 +8,32 @@ import java.util.Scanner;
 public class exercise20 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("What is the order amount?");
+        double orderamount = input.nextDouble();
+        System.out.println("What is the state?");
+        String state = input.next();
+        System.out.println("What is the county?");
+        String county = input.next();
+        double wiTax = .055;
+        double ilTax = .08;
+        if(state.equalsIgnoreCase("WI")) {
+            if(county.equalsIgnoreCase("EauClaire")){
+                wiTax = wiTax + .05;
+                System.out.printf("The subtotal is $%.2f\nThe tax is $%.2f\nThe total is $%.2f\n", orderamount, orderamount * wiTax, orderamount + (orderamount * wiTax));
+            }
+            else if(county.equalsIgnoreCase("Dunn")){
+                wiTax = wiTax + .04;
+                System.out.printf("The subtotal is $%.2f\nThe tax is $%.2f\nThe total is $%.2f\n", orderamount, orderamount * wiTax, orderamount + (orderamount * wiTax));
+            }
+            else{
+                System.out.printf("The subtotal is $%.2f\nThe tax is $%.2f\nThe total is $%.2f\n", orderamount, orderamount * wiTax, orderamount + (orderamount * wiTax));
+            }
+        }
+        else if(state.equalsIgnoreCase("IL")) {
+            System.out.printf("The subtotal is $%.2f\nThe tax is $%.2f\nThe total is $%.2f\n", orderamount, orderamount * ilTax, orderamount + (orderamount * ilTax));
+        }
+        else{
+            System.out.printf("The subtotal is $%.2f\nThe tax is $0.00\nThe total is $%.2f\n", orderamount, orderamount);
+        }
     }
 }
